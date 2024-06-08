e3d106c167ec9c516c63de73516a43c9d74f34fe
shariar-duke <shariarmahmud225@gmail.com>
2024-06-08 20:02:19 +0600

    Hotel List Component

---
+++ app/componensts/hotel/HotelList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HotelCard from "./HotelCard";
+
+export default function HotelList() {
+  return (
+    <div className="col-span-9">
+      <div className="space-y-4">
+        <HotelCard />
+      </div>
+    </div>
+  );
+}
