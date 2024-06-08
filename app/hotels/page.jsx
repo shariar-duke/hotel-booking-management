ee37fbbe3bbbdebf36655928d80f37fa4abb9866
shariar-duke <shariarmahmud225@gmail.com>
2024-06-08 19:42:12 +0600

    HotelList Page

---
+++ app/hotels/page.jsx
@@ -0,0 +1,20 @@
+import HotelList from "../componensts/hotel/HotelList";
+import Filter from "../componensts/search/Filter";
+import Search from "../componensts/search/Search";
+export default function HotelListPage() {
+  return (
+    <>
+      <section className="bg-[url('/hero-bg.jpg')] bg-cover bg-no-repeat bg-center pt-[100px] pb-[60px]">
+        <div className="container items-center py-12">
+          <Search fromList={true} />
+        </div>
+      </section>
+      <section className="py-12">
+        <div className="container grid grid-cols-12">
+          <Filter />
+          <HotelList/>
+        </div>
+      </section>
+    </>
+  );
+}
