1bf387e8d6bae9b1b2a1b41405f1ba0727aef0cb
shariar-duke <shariarmahmud225@gmail.com>
2024-06-09 00:03:24 +0600

    login page

---
+++ app/login/page.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import LoginForm from "../componensts/auth/LoginForm";
+import SocialLogin from "../componensts/auth/SocialLogin";
+
+export default function LoginPage() {
+  return (
+    <section className="h-screen grid place-items-center">
+      <div className="max-w-[450px] w-full mx-auto p-6 border border-gray-700/20 rounded-md">
+        <h4 className="font-bold text-2xl">Sign in</h4>
+        <LoginForm />
+        <SocialLogin />
+      </div>
+    </section>
+  );
+}
+
