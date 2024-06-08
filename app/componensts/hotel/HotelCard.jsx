9f8b749e68f885dc8d2d3625aa624b671a19a0fa
shariar-duke <shariarmahmud225@gmail.com>
2024-06-08 20:01:58 +0600

    Hotel Card component

---
+++ app/componensts/hotel/HotelCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import HotelSummaryInfo from './HotelSummaryInfo'
+
+export default function HotelCard() {
+  return (
+    <div className="flex gap-6 border border-gray/20 p-4 rounded-md">
+     
+      <HotelSummaryInfo />
+    </div>
+  )
+}
