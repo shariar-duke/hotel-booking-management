8504d0931e3e33d41fde2e3ac7942e16316179aa
shariar-duke <shariarmahmud225@gmail.com>
2024-06-09 10:20:23 +0600

    Regitration componenet

---
+++ app/register/page.jsx
@@ -0,0 +1,15 @@
+
+import RegistrationForm from '../componensts/auth/RegistrationForm'
+import SocialLogin from '../componensts/auth/SocialLogin'
+
+export default function RegistrationPage() {
+  return (
+    <section className="h-screen grid place-items-center mt-6">
+      <div className="max-w-[450px] w-full mx-auto p-6 border border-gray-700/20 rounded-md">
+        <h4 className="font-bold text-2xl">Sign up</h4>
+        <RegistrationForm />
+        <SocialLogin />
+      </div>
+    </section>
+  )
+}
