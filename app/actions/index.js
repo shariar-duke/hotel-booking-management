cb3c7855df45f2d384e48f35e4cb6fe74d93b096
shariar-duke <shariarmahmud225@gmail.com>
2024-06-10 18:10:38 +0600

    complete server action

--- app/actions/index.js
+++ app/actions/index.js
@@ -5,11 +5,13 @@ import { signIn } from "../../auth";
 export async function login(formData) {
   try {
     // eta k bolte hbe eta ki type signIn
-    await signIn("credentials", {
+    const response = await signIn("credentials", {
       email: formData.get("email"),
       password: formData.get("password"),
-      redirect:false // jode ei page a kno error ase tbuo o ei page a thakbo onno page a jawa hbe na 
+      redirect: false, // jode ei page a kno error ase tbuo o ei page a thakbo onno page a jawa hbe na
     });
+
+    return response; // ei response ta send na korar jnno error khaitecilam
   } catch (err) {
     throw new Error(err);
   }
