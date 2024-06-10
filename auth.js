144d77ef6f327427197c322ae4897d6620d28c12
shariar-duke <shariarmahmud225@gmail.com>
2024-06-10 16:45:25 +0600

    implementing login with google successfully

--- auth.js
+++ auth.js
@@ -1,8 +1,8 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 // import CredentialsProvider from "next-auth/providers/credentials";
-// import { MongoDBAdapter } from "@auth/mongodb-adapter";
-// import mongoClientPromise from "./database/mongoClientPromise";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import mongoClientPromise from "./database/mongoClientPromise";
 // import { userModel } from "./models/user-model";
 
 export const {
@@ -11,6 +11,9 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+
+   adapter: MongoDBAdapter(mongoClientPromise, {databaseName: process.env.ENVIRONMENT }),
+//    databasae name ta ENVIRONMENT variaable er modh ease env file a 
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
