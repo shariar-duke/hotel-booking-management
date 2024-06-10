5fc7da504dbf9217204b094861598c3631c81721
shariar-duke <shariarmahmud225@gmail.com>
2024-06-10 17:45:00 +0600

    necessary changes to integrate credential login

--- auth.js
+++ auth.js
@@ -1,9 +1,9 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-// import CredentialsProvider from "next-auth/providers/credentials";
+import CredentialsProvider from "next-auth/providers/credentials";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import mongoClientPromise from "./database/mongoClientPromise";
-// import { userModel } from "./models/user-model";
+import { userModel } from "./models/user-model";
 
 export const {
   handlers: { GET, POST },
@@ -11,10 +11,48 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  adapter: MongoDBAdapter(mongoClientPromise, {
+    databaseName: process.env.ENVIRONMENT,
+  }),
 
-   adapter: MongoDBAdapter(mongoClientPromise, {databaseName: process.env.ENVIRONMENT }),
-//    databasae name ta ENVIRONMENT variaable er modh ease env file a 
   providers: [
+    CredentialsProvider({
+      credentials: {
+        email: {}, // ei duta k empty rakhar karon holo amader kase already ui ase
+        password: {},
+      },
+      async authorize(credentials) {
+        if (credentials == null) {
+          return null; // ei ta tokhn hbe jokhn holo keu username and password ta dbei na tokhn.
+        }
+
+        try {
+          // user model theke amra email id dia anar try korbo j ase naki ei email. jode email thake tahoel pore emai r passwor ta match kore dekhb
+
+          const user = await userModel.findOne({ email: credentials.email });
+
+          if (user) {
+            // email ta abar milay dekhalm
+            const isMatch = user.email === credentials.email;
+
+            if (isMatch) {
+              return user;
+            } else {
+              throw new Error("Email or password mismatch");
+            }
+
+            // jode mile jay then...
+          } else {
+            throw new Error("User not found");
+          }
+
+          // jdoe user pia tahoel password ta mile dekhbo
+          // jdoe na pai tahle abar error throw kora jete pre ...
+        } catch (err) {
+          throw new Error(err);
+        }
+      },
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
