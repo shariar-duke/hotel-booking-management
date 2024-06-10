9e8769219f8f62be6c7d2253771284cc3a215994
shariar-duke <shariarmahmud225@gmail.com>
2024-06-10 15:53:22 +0600

    alternative text added for login and sign up

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -8,7 +8,7 @@ export default function LoginPage() {
       <div className="max-w-[450px] w-full mx-auto p-6 border border-gray-700/20 rounded-md">
         <h4 className="font-bold text-2xl">Sign in</h4>
         <LoginForm />
-        <SocialLogin />
+        <SocialLogin mode="login" />
       </div>
     </section>
   );
