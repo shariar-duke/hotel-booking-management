81108ea8724c561bdad8680a54fe9cd732fc3da8
shariar-duke <shariarmahmud225@gmail.com>
2024-06-11 08:52:15 +0600

    rotue handler for register

--- app/api/auth/register/route.js
+++ app/api/auth/register/route.js
@@ -1,5 +1,34 @@
-const route = () => {
-  return <div>route</div>;
-};
+import { userModel } from "@/models/user-model";
+import { dbConnect } from "@/service/mongo";
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+export const POST = async (request) => {
+  // assume kortse ei name a request a variable gula asbe
+  const { fname, lname, email, password } = await request.json();
 
-export default route;
+  // database connect
+  await dbConnect();
+  // ekhnae db connect ta r ekbar kortse karon amra vbtse j Ui charao jode keu functiona a perameter pass kore db te connect korar try kore...
+
+  // password ta hash korbo..
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  // ekhn user er ekta object banabo jeta amra model a inser korbo
+
+  const newUser = {
+    name: `${fname} ${lname}`,
+    email,
+    password: hashedPassword,
+  };
+
+  try {
+    await userModel.create(newUser);
+    return new NextResponse("User created successfully", {
+      status: 201,
+    });
+  } catch (error) {
+    return new NextResponse(err.message, {
+      status: 500,
+    });
+  }
+};
