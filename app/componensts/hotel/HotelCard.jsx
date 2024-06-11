4c0c971b143bf85e962b121e318822c9cb475dfd
shariar-duke <shariarmahmud225@gmail.com>
2024-06-11 11:05:32 +0600

    dynamic data for each hotel card

--- app/componensts/hotel/HotelCard.jsx
+++ app/componensts/hotel/HotelCard.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import HotelSummaryInfo from './HotelSummaryInfo'
+import Image from 'next/image'
 
-export default function HotelCard() {
+export default function HotelCard({hotelInfo}) {
   return (
     <div className="flex gap-6 border border-gray/20 p-4 rounded-md">
      
-      <HotelSummaryInfo />
+     <Image src={hotelInfo?.thumbNailUrl} alt="hotel image" width={240} height={160} />
+      <HotelSummaryInfo fromListPage={true} info ={hotelInfo} />
     </div>
   )
 }
--- app/componensts/hotel/HotelList.jsx
+++ app/componensts/hotel/HotelList.jsx
@@ -9,7 +9,13 @@ export default async function HotelList() {
   return (
     <div className="col-span-9">
       <div className="space-y-4">
-        <HotelCard />
+
+        {
+          allHotels.map((hotel) => (
+            <HotelCard key={hotel.id} hotelInfo={hotel} />
+          ))
+        }
+     
       </div>
     </div>
   );
