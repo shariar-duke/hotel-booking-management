ddd0905cbb499a4cc45abca792c7893377c4e5b0
shariar-duke <shariarmahmud225@gmail.com>
2024-06-11 11:04:57 +0600

    selecting data for hotels here should be an array

--- database/queries/index.js
+++ database/queries/index.js
@@ -4,9 +4,9 @@ import { replaceMongoIdInArray } from "@/utils/data-util";
 export async function getAllHotels() {
     const hotels = await hotelModel
     .find()
+    .select(["thumbNailUrl", "name", "highRate", "lowRate", "city", "propertyCategory"])
     .lean();
 
     return replaceMongoIdInArray(hotels);
 }
-
 // hotel model theke sob data tule anbe ei function and  object id gula k replace kore just id banabe 
