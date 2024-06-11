fd18a46ff6f40ba4dff683a11b12db1c3b321faf
shariar-duke <shariarmahmud225@gmail.com>
2024-06-11 11:05:11 +0600

    another image domian

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,11 @@ const nextConfig = {
           {
             protocol: 'https',
             hostname: 'lh3.googleusercontent.com',
+          },
+
+          {
+            protocol: 'https',
+            hostname: "a0.muscache.com",
           }
         ],
       }
