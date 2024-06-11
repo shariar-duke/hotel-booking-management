d9c1d89097195dce5c4e8a0180034fe5e10ca6ce
shariar-duke <shariarmahmud225@gmail.com>
2024-06-11 10:20:59 +0600

    route protected for booking page

--- app/(home)/bookings/page.jsx
+++ app/(home)/bookings/page.jsx
@@ -2,8 +2,19 @@ import React from "react";
 import ProfileInfo from "@/app/componensts/users/ProfileInfo";
 import PastBooking from "@/app/componensts/users/bookings/PastBooking";
 import UpcomingBooking from "@/app/componensts/users/bookings/UpcomingBooking";
+import {redirect} from "next/navigation"; // notice this redirect syntax 
 
-export default function BookingsPage() {
+import { auth } from "../../../auth";
+// auth k import kore niye asbo ..
+export default async function BookingsPage() {
+  // ekhne session ta pete pare...
+  const session = await auth();
+
+  // jode session na pai tahle redirect kore dbo holo login page  a 
+  if (!session) {
+      redirect("/login");
+  }
+
   return (
     <>
       <section className="mt-[100px]">
